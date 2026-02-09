fba2ed0ff31bb109571fe301ec2eb8c433bc4f9f
Vedant Goyal <vedantgoyal@Vedants-MacBook-Air.local>
2026-02-10 01:52:19 +0530

    make blog admin more attractive

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Configuration 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
---
+++ blogs/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Category
+from .models import Blog
+
+# Register your models here.
+
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title' , 'category', 'author', 'status', 'is_featured')
+    search_fields = ('id', 'title', 'category__category_name', 'status')
+    list_editable = ('is_featured',)
+
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
